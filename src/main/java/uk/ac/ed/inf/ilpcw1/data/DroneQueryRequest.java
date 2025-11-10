a70eeded3ac8a13f2ddc5c93bc295e60ac1a7d19
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-11-10 15:53:59 +0000

    FEAT - created DTO for dynamic queries for endpoints 3 & 4

--- src/main/java/uk/ac/ed/inf/ilpcw1/data/DroneQueryRequest.java
+++ src/main/java/uk/ac/ed/inf/ilpcw1/data/DroneQueryRequest.java
@@ -1,4 +1,25 @@
 package uk.ac.ed.inf.ilpcw1.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a query attribute for dynamic drone queries
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DroneQueryRequest {
+    @JsonProperty("attribute")
+    private String attribute;
+
+    @JsonProperty("operator")
+    private String operator;
+
+    @JsonProperty("value")
+    private String value;
 }
